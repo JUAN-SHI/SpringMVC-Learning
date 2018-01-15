7f3978b96747a6c87d5b57894d4ecb121f559965
SJuan <1361643223@qq.com>
2018-01-15 19:15:11 +0800

    Spring MVC表单处理

---
+++ FormHandling/src/com/www/springmvc/controller/Student.java
@@ -0,0 +1,34 @@
+package com.www.springmvc.controller;
+
+/**
+ * Created by shijuan on 2017/12/7.
+ */
+public class Student {
+    private Integer age;
+    private String  name;
+    private Integer id;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
